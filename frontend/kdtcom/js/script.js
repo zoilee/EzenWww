d52e90f50c25a40a2cb626541ec51d202fb97fa3
zoilee <ohsj2337@gmail.com>
2024-08-27 17:56:23 +0900

    240827

--- frontend/kdtcom/js/script.js
+++ frontend/kdtcom/js/script.js
@@ -1,9 +1,11 @@
 $(function(){
+
+    //갤러리 로딩
     $(window).on('load', function(){
         $('.photos-gallery').pycsLayout({
             pictureContainer:".picture",
             gutter:5,
-            idealHeight: 140
+            idealHeight: 150
         });
     })
 
